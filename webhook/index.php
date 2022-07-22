1bb96d91f22c2d8c1c04fd1c24c3f47a7e3c893c
gulinmar <marcelo@gulin.com.ar>
2022-07-22 10:52:44 -0300

    fixed webhook

--- webhook/index.php
+++ webhook/index.php
@@ -5,12 +5,12 @@
   // Converts it into a PHP object
   $data = json_decode($json);
 
-  $file = fopen("output.txt", "a") or die("Cannot open file.");
-  fwrite($file, "{$data}\n");
+  $file = fopen("data.json", "a") or die("Cannot open file.");
+  fwrite($file, $json);
   fclose($file);  
 
   // render json response
   header('Content-Type: application/json'); 
-  echo json_encode($data);
-
+  // echo json_encode($data);
+  echo $json;
 ?>
